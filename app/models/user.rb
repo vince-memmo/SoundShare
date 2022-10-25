e16194e8abe9a3cabc2af9264af83e91c651d46b
“Vince <vmemmo1@gmail.com>
2022-10-24 18:28:27 -0700

    user table and model

---
+++ app/models/user.rb
@@ -0,0 +1,63 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  before_validation :ensure_session_token
+  has_secure_password
+
+
+  validates :username, :email, :session_token, uniqueness:true
+  validates :username, length: { in: 3..30 }
+  validates :email, length: { in: 3..255 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, format: { without: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: {in: 6..255}, allow_nil:true
+  validates :session_token, presence:true
+
+  def self.find_by_credentials(credential, password)
+    if URI::MailTo::EMAIL_REGEXP.match(credential)
+      @user = User.find_by(email: credential)
+    else
+      @user = User.find_by(username: credential)
+    end
+
+    if @user && @user.authenticate(password)
+      @user
+    else
+        nil
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save! 
+    self.session_token
+  end
+
+  private
+
+  def generate_unique_session_token
+      valid_session_token = false
+
+      while valid_session_token == false
+        self.session_token = SecureRandom.urlsafe_base64
+        if User.exists?(self.session_token)
+          valid_session_token = true
+        end
+      end
+      self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+end
